2ef46f9f42a44c42eb58a3cddb8152eca5fb5380
mostaphasafasf <mostaphasafasf@gmail.com>
2017-03-29 19:16:34 +0100

    First website upload

--- JS/index.js
+++ JS/index.js
@@ -12,52 +12,14 @@ $(function(){
     $('html, body').delay(380).animate({scrollTop:800}, 'slow');
     return false;
 	});	
-	$('#featuresbeforefooter').click(function () {
+			$('#featuresbeforefooter').click(function () {
     $('html, body').delay(380).animate({scrollTop:800}, 'slow');
     return false;
-	});	
-	$('#btnhbt1').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:850}, 'slow');
-    	return false;
-	});	
-	$('#btnhbt2').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:850}, 'slow');
-    	return false;
-	});	
-	$('#btnhbt3').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:850}, 'slow');
-    	return false;
-	});	
-
-
-	$('#btnhbt01').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:1400}, 'slow');
-    	return false;
-	});	
-	$('#btnhbt02').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:1400}, 'slow');
-    	return false;
-	});	
-	$('#btnhbt03').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:1400}, 'slow');
-    	return false;
-	});	
-
-
-	$('#btnhbt00').click(function () {
-   	 $('html, body').delay(380).animate({scrollTop:2000}, 'slow');
-    	return false;
 	});	
 
 			/* ...............................respensive..........................*/
 			$('#navicon').click(function(){
-				$('.resnavbar').slideToggle('slow')
+				$('.navlili').css({'width':'60%'})
 
 			});
-				$('.discover').click(function () {
-    			$('html, body').delay(380).animate({scrollTop:754}, 'slow');
-   				 return false;
-				});	
-
-
 });
