d31ebda9918db4e420508f2b79b49bb9fb3083d8
mostaphasafasf <mostaphasafasf@gmail.com>
2017-03-29 18:55:28 +0100

    Fisrt website upload

---
+++ JS/index.js
@@ -0,0 +1,63 @@
+$(function(){
+	
+	$('#about').click(function () {
+    $('html, body').delay(380).animate({scrollTop:500}, 'slow');
+    return false;
+	});			
+	$('#features').click(function () {
+    $('html, body').delay(380).animate({scrollTop:2000}, 'slow');
+    return false;
+	});	
+		$('#slidebuttoni').click(function () {
+    $('html, body').delay(380).animate({scrollTop:800}, 'slow');
+    return false;
+	});	
+	$('#featuresbeforefooter').click(function () {
+    $('html, body').delay(380).animate({scrollTop:800}, 'slow');
+    return false;
+	});	
+	$('#btnhbt1').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:850}, 'slow');
+    	return false;
+	});	
+	$('#btnhbt2').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:850}, 'slow');
+    	return false;
+	});	
+	$('#btnhbt3').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:850}, 'slow');
+    	return false;
+	});	
+
+
+	$('#btnhbt01').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:1400}, 'slow');
+    	return false;
+	});	
+	$('#btnhbt02').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:1400}, 'slow');
+    	return false;
+	});	
+	$('#btnhbt03').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:1400}, 'slow');
+    	return false;
+	});	
+
+
+	$('#btnhbt00').click(function () {
+   	 $('html, body').delay(380).animate({scrollTop:2000}, 'slow');
+    	return false;
+	});	
+
+			/* ...............................respensive..........................*/
+			$('#navicon').click(function(){
+				$('.resnavbar').slideToggle('slow')
+
+			});
+				$('.discover').click(function () {
+    			$('html, body').delay(380).animate({scrollTop:754}, 'slow');
+   				 return false;
+				});	
+
+
+});
